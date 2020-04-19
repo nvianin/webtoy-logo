24ee129973b5009cd8ee8e6599dbd8678a77ae4d
nvianin <n.vianin@gmail.com>
2020-04-19 19:29:39 +0200

    beginning csg

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
+var y, a, s;
+var yCSG, aCSG, sCSG;
+
 window.addEventListener("DOMContentLoaded", function () {
   var canvas = document.getElementById("renderCanvas");
   var engine = new BABYLON.Engine(canvas, true);
+  var scene = new BABYLON.Scene(engine);
+  var camera = new BABYLON.ArcRotateCamera(
+    "camera",
+    -1.85,
+    1.2,
+    10,
+    new BABYLON.Vector3(0, 5, -10),
+    scene
+  );
+  camera.setTarget(BABYLON.Vector3.Zero());
+  camera.attachControl(canvas, false);
+
+  BABYLON.SceneLoader.ImportMesh(
+    "",
+    "./",
+    "yas_letters_export.babylon",
+    scene,
+    function (newMeshes) {
+      y = newMeshes[2];
+      a = newMeshes[0];
+      s = newMeshes[1];
+      camera.target = a;
+      yCSG = BABYLON.CSG.FromMesh(y);
+      aCSG = BABYLON.CSG.FromMesh(a);
+      sCSG = BABYLON.CSG.FromMesh(s);
+    }
+  );
 
   var createScene = function () {
-    var scene = new BABYLON.Scene(engine);
-    var camera = new BABYLON.FreeCamera(
-      "camera",
-      new BABYLON.Vector3(0, 5, -10),
-      scene
-    );
-    camera.setTarget(BABYLON.Vector3.Zero());
-    camera.attachControl(canvas, false);
     var light = new BABYLON.HemisphericLight(
       "light1",
       new BABYLON.Vector3(0, 1, 0),
       scene
     );
-    var sphere = BABYLON.MeshBuilder.CreateSphere(
+    /* var sphere = BABYLON.MeshBuilder.CreateSphere(
       "sphere",
       { segments: 16, diameter: 2 },
       scene
     );
-    sphere.position.y = 1;
-    var ground = BABYLON.MeshBuilder.CreateGround(
+    sphere.position.y = 1; */
+    /* var ground = BABYLON.MeshBuilder.CreateGround(
       "ground1",
       { height: 6, width: 6, subdivision: 2 },
       scene
-    );
+    ); */
+
+    /* var Loaded = false;
+    while (!Loaded) {
+      if (y != null && s != null && a != null) {
+        
+        loaded = true;
+      }
+    } */
+
     return scene;
   };
 
@@ -34,9 +65,23 @@ window.addEventListener("DOMContentLoaded", function () {
 
   engine.runRenderLoop(function () {
     scene.render();
+    update();
   });
 
   window.addEventListener("resize", function () {
     engine.resize();
   });
 });
+
+function update(){
+  yCSG.position = y.position;
+  yCSG.rotation = y.rotation;
+}
+
+function toDeg(radians){
+  return radians*57.2958;
+}
+
+function toRad(degrees){
+  return degrees*0.0174533;
+}
